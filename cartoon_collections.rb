e2a6826ebf38b046361929c284ecd1bed0c441a7
Mason Waller <masonwaller805@gmail.com>
2019-10-16 23:19:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(veggies)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
+ 
   # Your code here
 end
 
