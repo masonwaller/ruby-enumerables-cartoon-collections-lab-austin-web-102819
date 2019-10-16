1c7475d9ffc84fa25eb3db35da2ba806951d5bff
Mason Waller <masonwaller805@gmail.com>
2019-10-16 23:39:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
+names.each_with_index { |n, index| puts "#{index+1}. #{n}"}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
+  array=[]
+  veggies.map { |n| array.push(n.capitalize+"!") }
+  return array
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
+ calls.any? { |n| n.length > 4 }
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+  mason=[]
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? { |n|  mason.push(n) if cheese_types.include?(n) }
+  return mason[0]
 end
